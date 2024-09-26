45a5ef13832d499f4cbbfc4ba096acbb1bfc1a12
Johan Swanepoel <johannes.swanepoel@gmail.com>
2024-09-26 19:45:04 +1000

    looping working well and also hiding of file browser

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ log = logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)
 
 window_height = 700
+height_adjust = 70
 image_suffixes = ("jpg", "jpeg", "gif", "webp")
 video_suffixes = ("mp4", "webm")
 # app = Flask(__name__, static_url_path="", static_folder="templates")
@@ -48,19 +49,23 @@ def play_current_file():
 def play_next_file():
     global g_file_list
     global g_current_file
-    next_file = utils.find_next_item(g_file_list, g_current_file)
-    return play_file_return(next_file['path'], next_file['id'])
+    if g_current_file != "":
+        next_file = utils.find_next_item(g_file_list, g_current_file)
+        return play_file_return(next_file['path'], next_file['id'])
+    else:
+        return "Watch this space ... "
 
 def play_file_return(request_path, file_id):
     global g_current_file
     global window_height
+    global g_loop_time
     g_current_file = file_id
     if request_path.endswith(image_suffixes):
         img = Image.open(request_path) 
         # check if it is an image or video
-        return render_template("showimage.html", image=request_path, height=window_height, file_name=file_id)
+        return render_template("showimage.html", image=request_path, height=window_height, file_name=file_id, loop_time=g_loop_time)
     elif request_path.endswith(video_suffixes):
-        return render_template("playvideo.html", image=request_path, height=window_height, file_name=file_id)
+        return render_template("playvideo.html", image=request_path, height=window_height, file_name=file_id, loop_time=g_loop_time)
     else:
         abort(404)
 
@@ -68,8 +73,18 @@ def play_file_return(request_path, file_id):
 def update_height():
     global window_height
     height = request.json['height']
-    window_height = height - 100
+    window_height = height - height_adjust
     return "{result: True}"
+
+@app.route("/loop_time", methods=['POST','GET'])
+def loop_time():
+    global g_loop_time
+    if request.method == 'POST':
+        form_loop_time = request.values.get('loop_time')
+        g_loop_time = form_loop_time
+    return render_template("loop_time.html", loop_time = g_loop_time)
+    
+
 
 @app.route("/file_listing", methods=['POST','GET'])
 def get_file_listing():
